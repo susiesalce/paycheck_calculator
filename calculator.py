274777342c28cf9c14cd07b42874821a05dc9405
Susie Salce <susanne.salce@gmail.com>
2021-08-29 18:13:51 +0000

    fix code commit

--- calculator.py
+++ calculator.py
@@ -7,7 +7,7 @@ bills_to_pay = input()
 print("You currently have {} bills to pay. Is this correct? Y/N".format(bills_to_pay))
 response = input ()  
 if response == 'Y':
-    print("The left over money is $" + str(int(pay_amount) - int(bills_to_pay)) + " dollars. Happy spending!")
+    print("The left over money is $" + str(round(float(pay_amount) - float(bills_to_pay), 2)) + " dollars. Happy spending!")
 elif response == 'N':
     print("Please input the correct amount ammount")
     bills_to_pay = input()
